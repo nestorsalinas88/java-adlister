d4ab5866c7eea3e642f982672612048ef669d8f0
nestor salinas <nestorasm88@gmail.com>
2019-08-27 12:10:05 -0500

    moving on

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -2,6 +2,10 @@ package config;
 
 public class Config {
 
+//    We need to set our thing up
+    private static String url ;
+    private static String username;
+    private static String password ;
 
     public static String getPassword() {
         return password;
